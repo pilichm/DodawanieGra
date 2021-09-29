c3377da0e3cebab158553dbc7c28ddb1d4bb9286
pilichm <michal.pilichowski@onet.pl>
2021-09-29 18:22:34 +0200

    Added: - initial layout creation.

--- app/src/main/java/pl/pilichm/dodawaniegra/MainActivity.kt
+++ app/src/main/java/pl/pilichm/dodawaniegra/MainActivity.kt
@@ -2,10 +2,26 @@ package pl.pilichm.dodawaniegra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpListeners()
     }
+
+    private fun setUpListeners(){
+        /**
+         * Start game, hide start button, start timer and display first question.
+         * */
+        tvStart.setOnClickListener {
+            tvStart.visibility = View.INVISIBLE
+            frAnswers.visibility = View.VISIBLE
+        }
+    }
+
+
 }
